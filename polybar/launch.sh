d634ec20eb40cb1372174900b86d7cee87aef25f
LaTex <32957066+junjiecjj@users.noreply.github.com>
2021-04-16 16:17:40 +0800

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$HOME/.config/polybar/launch.sh 内容如下：
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -9,12 +9,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar mybar &
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
 echo "Polybar launched..."
-
 
 # chmod +x $HOME/.config/polybar/launch.sh
