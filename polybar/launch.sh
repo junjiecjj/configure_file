ad2db09c397978be3e02bc502601424cb6e447a6
LaTex <32957066+junjiecjj@users.noreply.github.com>
2021-04-16 20:17:37 +0800

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
+polybar jackbar &
 
 echo "Polybar launched..."
-
-# chmod +x $HOME/.config/polybar/launch.sh
